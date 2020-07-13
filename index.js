74ef03870eeece6395d334a4f840186a2c7c4418
Badri <programmingwithbadri@outlook.com>
2020-07-13 14:39:42 +0530

    Handling the comment moderation

--- index.js
+++ index.js
@@ -7,7 +7,24 @@ app.use(bodyParser.json());
 
 // Recieve events from event bus
 app.post("/events", async (req, res) => {
-  // TODO
+  const { type, data } = req.body;
+
+  // if event type is commentcreated check if the comment has word "Orange"
+  if (type === "CommentCreated") {
+    const status = data.content.includes("orange") ? "rejected" : "approved";
+
+    // Emit the new event to event bus that the comment is moderated and the status is updated
+    await axios.post("http://localhost:4005/events", {
+      type: "CommentModerated",
+      data: {
+        id: data.id,
+        postId: data.postId,
+        status,
+        content: data.content,
+      },
+    });
+  }
+
   res.send({});
 });
 
